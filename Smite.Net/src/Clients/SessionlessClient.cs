a35d5a3bf83cbd01f7ae33d63921cce5114839ca
Casino Boyale <kayrenhi@gmail.com>
2019-03-22 13:59:11 +0000

    Added SessionlessClient

---
+++ Smite.Net/src/Clients/SessionlessClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+
+namespace Smite.Net.Clients
+{
+    public sealed class SessionlessClient
+    {
+        private readonly string _devKey;
+
+        public SessionlessClient(string devKey)
+        {
+            _devKey = devKey;
+        }
+
+        public static async Task<SmiteClient> CreateAsync(string devKey)
+        {
+            var authKey = "";
+
+            return new SmiteClient(devKey, authKey);
+        }
+
+        public async Task<SmiteClient> CreateClientAsync()
+        {
+            var client = await CreateAsync(_devKey).ConfigureAwait(false);
+            return client;
+        }
+    }
+}
